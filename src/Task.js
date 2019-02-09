79f30def92e869c764a37dc69ec8db552ea26967
Robert Kwasnicki <rkwas005@wp.pl>
2019-02-09 10:15:22 +0100

    Checkboxes with some code

--- src/Task.js
+++ src/Task.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react';
 
 class Task extends Component {
 
+    handleChange = (event) => {
+        console.log('Kliknięto mnie')
+        console.log(event.target.checked)
+    }
+
     render() {
         const styles = this.props.mruczek ? {textDecoration: 'line-through'} : {}
         return (
             <div>
-                <p style={styles}>{this.props.fafik} <input type='checkbox'></input> </p>
+                <p style={styles}>
+                    <input
+                        type='checkbox'
+                        checked={this.props.mruczek}
+                        onChange={this.handleChange}
+
+                    />
+                    {this.props.fafik}
+                </p>
             </div>
         )
     }
