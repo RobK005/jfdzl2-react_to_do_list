28eefd0f29f508695c96044d42c6d790cec317e9
Robert Kwasnicki <rkwas005@wp.pl>
2019-02-09 09:26:47 +0100

    Task.js

---
+++ src/Task.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Task extends Component {
+
+    render() {
+        return (
+            <div>
+                {this.props.fafik}
+            </div>
+        )
+    }
+}
+
+export default Task
--- src/TaskContainer.js
+++ src/TaskContainer.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
+import Task from './Task'
 
 class TaskContainer extends Component {
 
     state = {
         day: 'sobota',
         tasks: [
-            { id: 1, name:'Zakupy', complated: false},
-            { id: 2, name:'Odkurzyć', complated: false},
-            { id: 3, name:'Nauczyć Reacta', complated: false},
+            { id: 1, name:'Zakupy', completed: false},
+            { id: 2, name:'Odkurzyć', completed: false},
+            { id: 3, name:'Nauczyć Reacta', completed: false},
         ]
     }
 
     render() {
         return (
             <div>
+
+                {/* <Task fafik='Zakupy'/>
+                <Task fafik='Odkurzyć'/>
+                <Task fafik='Nauczyć Reacta'/> */}
+
+                {/* ten sam zapis jest ponizej z metoda map */}
+
                 {this.state.tasks.map((task, index) => (
-                    <p key={`t-${index}`}>{task.name}</p>
+                    <Task key={`t-${task.id}`} fafik={task.name}/>
                 ))}
             </div>
         )
