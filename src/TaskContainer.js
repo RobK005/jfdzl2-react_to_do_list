d038d976e70f58cae925e79daf76431a640ea279
Robert Kwasnicki <rkwas005@wp.pl>
2019-02-09 09:30:25 +0100

    Task.js - corrected

--- src/TaskContainer.js
+++ src/TaskContainer.js
@@ -8,7 +8,7 @@ class TaskContainer extends Component {
         tasks: [
             { id: 1, name:'Zakupy', completed: false},
             { id: 2, name:'Odkurzyć', completed: false},
-            { id: 3, name:'Nauczyć Reacta', completed: false},
+            { id: 3, name:'Nauczyć Reacta', completed: false}
         ]
     }
 
@@ -20,9 +20,9 @@ class TaskContainer extends Component {
                 <Task fafik='Odkurzyć'/>
                 <Task fafik='Nauczyć Reacta'/> */}
 
-                {/* ten sam zapis jest ponizej z metoda map */}
+                {/* --podobny zapis jest ponizej z metoda map-- */}
 
-                {this.state.tasks.map((task, index) => (
+                {this.state.tasks.map(task => (
                     <Task key={`t-${task.id}`} fafik={task.name}/>
                 ))}
             </div>
